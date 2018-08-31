7f8b73d607e8c519c8a48a7e07eef0d1fda1ed96
Nikolay Ilchenko <crabonuggets@gmail.com>
2018-08-31 18:43:29 +0600

    Лекция 5.2

--- addressbook-web-tests/addressbook-web-tests/appmanager/GroupHelper.cs
+++ addressbook-web-tests/addressbook-web-tests/appmanager/GroupHelper.cs
@@ -128,10 +128,26 @@ namespace WebAddressbookTests
 
                 foreach (IWebElement element in elements)
                 {
-                    groupCache.Add(new GroupData(element.Text)
+                    groupCache.Add(new GroupData(null)
                     {
                         Id = element.FindElement(By.TagName("input")).GetAttribute("value")
                     });
+                }
+
+                string allGroupNames = driver.FindElement(By.CssSelector("div#content form")).Text;
+                string [] parts = allGroupNames.Split('\n'); //Разрезает строку на несколько строк по разделителю "перевод строки"
+                int shift = groupCache.Count - parts.Length;
+                for (int i = 0; i < groupCache.Count; i++)
+                {
+                    if(i < shift)
+                    {
+                        groupCache[i].Name = "";
+                    }
+                    else
+                    {
+                        groupCache[i].Name = parts[i-shift].Trim();
+
+                    }
                 }
             }
 
