729ef2886c96e335217a80886510ab2762bd84c5
Nikolay Ilchenko <crabonuggets@gmail.com>
2018-09-12 00:32:44 +0600

    Половина 6.2

--- addressbook-web-tests/addressbook-test-data-generators/Program.cs
+++ addressbook-web-tests/addressbook-test-data-generators/Program.cs
@@ -14,14 +14,53 @@ namespace addressbook_test_data_generators
         {
             int count = Convert.ToInt32(args[0]);
             StreamWriter writer = new StreamWriter(args[1]);
+            string format = args[3];
+
+            List<GroupData> groups = new List<GroupData>();
+
+
             for (int i = 0; i < count; i++)
             {
+                groups.Add(new GroupData(TestBase.GenerateRandomString(10))
+                {
+                    Header = TestBase.GenerateRandomString(10),
+                    Footer = TestBase.GenerateRandomString(10)
+                });
                 writer.WriteLine(String.Format("${0},${1},${2}",
                     TestBase.GenerateRandomString(10),
                     TestBase.GenerateRandomString(10),
                     TestBase.GenerateRandomString(10)));
             }
+
+            if(format == "csv")
+            {
+                WriteGroupsToCsvFile(groups, writer);
+            }
+            else if(format == "xml")
+            {
+                WriteGroupsToXmlFile(groups, writer);
+            }
+            else
+            {
+                System.Console.Out.Write("Unrecognized format " + format);
+            }
+
             writer.Close();
+        }
+
+        static void WriteGroupsToCsvFile(List<GroupData> groups, StreamWriter writer)
+        {
+            foreach(GroupData group in groups)
+            {
+                writer.WriteLine(String.Format("${0},${1},${2}",
+                    group.Name, group.Header, group.Footer));
+
+            }
+        }
+
+        static void WriteGroupsToXmlFile(List<GroupData> groups)
+        {
+
         }
     }
 }
