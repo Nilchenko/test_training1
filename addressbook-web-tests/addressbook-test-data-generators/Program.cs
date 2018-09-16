acb99fc51b9f091408f3379afee5bffd3ea006d1
Nikolay Ilchenko <crabonuggets@gmail.com>
2018-09-17 00:38:24 +0600

    Добавлена запись в xlsx

--- addressbook-web-tests/addressbook-test-data-generators/Program.cs
+++ addressbook-web-tests/addressbook-test-data-generators/Program.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using System.Xml;
 using System.Xml.Serialization;
 using Newtonsoft.Json;
+using Excel = Microsoft.Office.Interop.Excel; // задали синоним, все обращения должны начинаться префикса
 using WebAddressbookTests;
 
 namespace addressbook_test_data_generators
@@ -16,7 +17,7 @@ namespace addressbook_test_data_generators
         static void Main(string[] args)
         {
             int count = Convert.ToInt32(args[0]);
-            StreamWriter writer = new StreamWriter(args[1]);
+            string fileName = args[1];
             string format = args[2];
 
             List<GroupData> groups = new List<GroupData>();
@@ -31,25 +32,62 @@ namespace addressbook_test_data_generators
                 });
             }
 
-            if (format == "csv")
+            if (format == "xlsx")
             {
-                WriteGroupsToCsvFile(groups, writer);
-            }
-            else if (format == "xml")
-            {
-                WriteGroupsToXmlFile(groups, writer);
-            }
-            else if (format == "json")
-            {
-                WriteGroupsToJsonFile(groups, writer);
+                WriteGroupsToXlsxFile(groups, fileName);
+
             }
 
             else
             {
-                System.Console.Out.Write("Unrecognized format " + format);
+                StreamWriter writer = new StreamWriter(fileName);
+
+                if (format == "csv")
+                {
+                    WriteGroupsToCsvFile(groups, writer);
+                }
+                else if (format == "xml")
+                {
+                    WriteGroupsToXmlFile(groups, writer);
+                }
+                else if (format == "json")
+                {
+                    WriteGroupsToJsonFile(groups, writer);
+                }
+
+                else
+                {
+                    System.Console.Out.Write("Unrecognized format " + format);
+                }
+
+                writer.Close();
             }
 
-            writer.Close();
+        }
+
+        static void WriteGroupsToXlsxFile(List<GroupData> groups, string fileName)
+        {
+            Excel.Application app = new Excel.Application(); //запускает эксель
+            app.Visible = true;  //позволяет смотреть, что происходит в окне приложения Excel
+            Excel.Workbook wb = app.Workbooks.Add(); //открыли документ
+            Excel.Worksheet sheet = wb.ActiveSheet; //получили открытую по умолчанию страницу
+            //sheet.Cells[1, 1] = "test";
+
+            int row = 1;
+            foreach (GroupData group in groups)
+            {
+                sheet.Cells[row, 1] = group.Name;
+                sheet.Cells[row, 2] = group.Header;
+                sheet.Cells[row, 3] = group.Footer;
+
+                row++;
+            }
+            string fullPath = Path.Combine(Directory.GetCurrentDirectory(), fileName);
+            File.Delete(fullPath);
+            wb.SaveAs(fullPath);
+
+            wb.Close();
+            app.Visible = false;
         }
 
         static void WriteGroupsToCsvFile(List<GroupData> groups, StreamWriter writer)
